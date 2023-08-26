0d16551561867e5c5b747f7dd669668f3eb41eb9
C-Feldt <cfeldt1@lsu.edu>
2023-08-26 17:37:41 -0500

    Created Bootbar.js as an alternate to Sidebar; Uploaded Beta Logo
    Created Bootbar.js using Bootstrap's NavBar to replace the existing NavBar.

--- client/src/components/Bootbar.js
+++ client/src/components/Bootbar.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import ULegislateLogo from "./ULegislate_BetaLogo.svg";
+import './styles/Bootbar.css'
 
 const Bootbar = () => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  
-    const handleDropdownToggle = () => {
-      setIsDropdownOpen(!isDropdownOpen);
-    };
-  
+
     return (
-      <Navbar>
-        
+        <Navbar expand="md" className="bootbar">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src={ULegislateLogo}
+              height="30"
+              className="d-inline-block align-top"
+              alt="ULegislate Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/draft">Draft Legislation</Nav.Link>
+              <Nav.Link href="/draftlist">Draft List</Nav.Link>
+              <Nav.Link href="/reviewdraft">Review Draft</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     );
   };
