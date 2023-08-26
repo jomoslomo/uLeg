6004e7bb088a0dd6ef009b1715f4bc45dd6fe2de
C-Feldt <cfeldt1@lsu.edu>
2023-08-26 18:01:29 -0500

    Added Placeholder Signed-In Object
    Added text for a future signed-in component.

--- client/src/components/Bootbar.js
+++ client/src/components/Bootbar.js
@@ -9,12 +9,12 @@ import './styles/Bootbar.css'
 const Bootbar = () => {
 
     return (
-        <Navbar expand="md" className="bootbar">
+        <Navbar expand="lg" className="bootbar">
         <Container>
           <Navbar.Brand href="/">
             <img
               src={ULegislateLogo}
-              height="30"
+              height="36"
               className="d-inline-block align-top"
               alt="ULegislate Logo"
             />
@@ -22,11 +22,15 @@ const Bootbar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/draft">Draft Legislation</Nav.Link>
-              <Nav.Link href="/draftlist">Draft List</Nav.Link>
-              <Nav.Link href="/reviewdraft">Review Draft</Nav.Link>
+              <Nav.Link className="options" href="/">Home</Nav.Link>
+              <Nav.Link className="options" href="/draft">Draft Legislation</Nav.Link>
+              <Nav.Link className="options" href="/draftlist">Draft List</Nav.Link>
+              <Nav.Link className="options" href="/reviewdraft">Review Draft</Nav.Link>
             </Nav>
+            {/* This Navbar.Text object is a placeholder until we have a sign-in component. */}
+            <Navbar.Text className="justify-content-end">
+                Welcome, &#60;Username&#62;! 
+            </Navbar.Text>
           </Navbar.Collapse>
         </Container>
       </Navbar>
